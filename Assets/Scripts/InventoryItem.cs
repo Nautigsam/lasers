f9332e0d32e9f80e9170907e303ea6771382dea5
Aurélien Bertron <aurelien.bertron@insa-lyon.fr>
2016-01-16 20:31:37 +0200

    adds inventory

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void InventoryItemClickAction (LaserType type);
+
+public class InventoryItem : MonoBehaviour {
+
+	public LaserType type;
+
+	public InventoryItemClickAction onLeftClick;
+	public InventoryItemClickAction onRightClick;
+
+	public void OnClick () {
+		if (Input.GetMouseButton (0)) {
+			onLeftClick (type);
+		} else if (Input.GetMouseButton (1)) {
+			onRightClick (type);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
